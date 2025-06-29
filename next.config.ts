74b8ce121a3d86cce9b9ad9b65ed87af92426081
kiranjeetkour144@gmail.com <kiranjeet kour>
2025-06-29 16:41:53 +0530

    refactor: update Prisma schema and remove User model
    feat: add new 3D model for keyboard
    
    style: enhance global styles with spotlight animation
    
    chore: update layout metadata and apply dark mode styles
    
    feat: restructure Home page to include Navbar and Keyboard components
    
    refactor: rename NavbarDemo to NavbarMain and update nav items
    
    feat: create Spotlight component for animated effects
    
    chore: add Prisma client initialization in a new lib file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['three'],
+  webpack: (config: Configuration) => {
+    if (!config.externals) config.externals = [];
+    if (Array.isArray(config.externals)) {
+      config.externals.push({
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'bufferutil': 'commonjs bufferutil',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
