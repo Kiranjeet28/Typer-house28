cf5d10d1ef3f00410841581db63bf59c601523a9
kiranjeetkour144@gmail.com <kiranjeet kour>
2025-07-29 14:56:29 +0530

    feat: restore NavbarMain and Footer components in RootLayout for improved layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
       >
         <Providers>
-          {/* <NavbarMain /> */}
+          <NavbarMain />
           {children}
-          {/* <Footer /> */}
+          <Footer />
         </Providers>
       </body>
     </html>
