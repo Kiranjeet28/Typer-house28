5f6182b1f14fd5b11cb699af4db87456b105c29e
Cipher <kiranjeetkour144@gmail.com>
2025-12-30 09:27:21 +0530

    fix: correct formatting in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,18 @@ import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['three'],
+
   webpack: (config: Configuration) => {
     if (!config.externals) config.externals = [];
     if (Array.isArray(config.externals)) {
       config.externals.push({
         'utf-8-validate': 'commonjs utf-8-validate',
-        'bufferutil': 'commonjs bufferutil',
+        bufferutil: 'commonjs bufferutil',
       });
     }
     return config;
   },
+
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
